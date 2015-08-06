b9ab83c7755f470ef29c143bf224933b5ec3dca7
Jordan Jordanov <jordanovsj@gmail.com>
2015-08-06 14:37:03 +0300

    Finish header_in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,19 @@
 class StaticPagesController < ApplicationController
+	
+	def home
+		 
+	end
+	
+	def front_page_text
+			#@front_page_text_partial=params[:info_partial]
+			render 'home'
+			#redirect_to root_url
+	end
+	
+	def about
+	end
+	
+	private
+
 end
+
