d5bd48dd83f39107cd7377682da622f5a16f3c7b
a-sklyarov <sasho.alex.sk@gmail.com>
2015-08-21 16:55:59 +0300

    1000 hours of pain to get the faye competitions show to work.

--- app/assets/javascripts/competition_views.js
+++ app/assets/javascripts/competition_views.js
@@ -1,6 +1,31 @@
-$(function() {
-  faye = new Faye.Client('/faye');
-  faye.subscribe('/competitions/_show', function(data) { 
-		eval(data);
-	});
-});
+//~ $(function(){
+	//~ var faye = new Faye.Client('/faye');
+	//~ faye.subscribe('/competitions', function(message) {
+		//~ $("#main-area").html(message.mess);
+	//~ });
+//~ });
+
+
+//~ function subscribe_competition(faye, competition_id) {
+	//~ subscriber =  faye.subscribe('/competitions', function(message) { 
+		//~ $("#competition-show").html(message.mess);
+	//~ });
+//~ }
+
+//~ $(function() {  
+	//~ 
+  //~ function subscribe_competition() {
+		//~ var faye = new Faye.Client('/faye');
+		//~ faye.subscribe('/competitions', function(message) { 
+			//~ $("#main-area").html(message.mess);
+		//~ });
+	//~ }
+	//~ 
+//~ });
+
+//~ $(function() {
+  //~ var faye = new Faye.Client('/faye');
+  //~ faye.subscribe('/competitions', function(message) { 
+		//~ $("#competition-show").html(message.mess);
+	//~ });
+//~ });
--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -6,15 +6,8 @@ class CompetitionsController < ApplicationController
 		@competition=Competition.find(params[:id])
 		if @competition.users.count < @competition.n_players
 			current_user.update_attributes!(competition_id: params[:id])
-			respond_to do |format|
-				format.js
-				format.html
-			end
 		else
-			respond_to do |format|
-				format.js
-				format.html {redirect_to root_path }
-			end
+			redirect_to root_path
 		end
   end
   
