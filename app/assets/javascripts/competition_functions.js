d6880d0f2946793a72ad4958e13e415097554e3f
a-sklyarov <sasho.alex.sk@gmail.com>
2015-08-25 00:39:45 +0300

    Added comments to the Faye stuff.

--- app/assets/javascripts/competition_functions.js
+++ app/assets/javascripts/competition_functions.js
@@ -1,3 +1,5 @@
+//~ Used for the Faye stuff. Reloads the page only in the timer is not on the page.
+//~ Otherwise, renders the message.mess partial in message.component DOM element.
 function change_view(message) {
 	if ( message.mess == "reload" && !( $("#competition-timer").length ) ) {
 		location.reload();
