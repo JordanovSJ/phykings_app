1fff61e728c5b53c039c2f9c5ef27ecfa7493500
a-sklyarov <sasho.alex.sk@gmail.com>
2015-08-25 00:25:07 +0300

    Made pretty much all of the available faye stuff work correctly. Still need to add comments.

--- app/assets/javascripts/competition_functions.js
+++ app/assets/javascripts/competition_functions.js
@@ -1,3 +1,13 @@
-function competition_show(message) {
-	$("#competition-show").html(message.mess);
+function change_view(message) {
+	if ( message.mess == "reload" && !( $("#competition-timer").length ) ) {
+		location.reload();
+	} else if ( message.mess != "reload" ) {
+		if ( $(message.component).length ) {
+			$(message.component).html(message.mess);
+		}
+	}
 }
+
+//~ $(message.component).load(location.href + " " + message.component + ">*", function() {
+			//~ MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+		//~ });
