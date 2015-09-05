973481f103564c32a419e51e19ad45e29a00d7c3
Jordan Jordanov <jordanovsj@gmail.com>
2015-09-05 14:43:50 +0300

    added 20 problems

---
+++ db/custom_seeds/problems_seeds.rb
@@ -0,0 +1,362 @@
+#10 minute problems ########################################################
+#####################
+#1
+Problem.create!(
+							title: "ship and wind",
+							answer: 397,
+							degree_of_answer: -1,
+							units_of_answer: "km/hour",
+							category: "Classical mechanics",
+							difficulty: 1,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "A ship moves along the equator to the east with velocity $$
+v_o = 30  km/hour $$. The southeastern wind blows at an angle $ \alpha = 60° $
+to the equator with velocity $ v = 15 $ km/hour . Find the wind velocity
+$ v_1 $ relative to the ship in the reference frame fixed to the ship.
+
+	Hint: The wind blows from south-east to nort-west!!!",
+)
+
+#2
+Problem.create!(
+							title: "three poitns",
+							answer: 4,
+							degree_of_answer: 1,
+							units_of_answer: "s",
+							category: "Classical mechanics",
+							difficulty: 4,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "Three points are located at the vertices of an equilateral
+triangle whose side equals $ 6 m $. They all start moving simultaneously
+with velocity $ 3,6 km/hour $ constant in modulus, with the first point heading
+continually for the second, the second for the third, and the third
+for the first. How soon will the points converge?",
+)
+#3
+Problem.create!(
+							title: "cat and mouse",
+							answer: 45,
+							degree_of_answer: -1,
+							units_of_answer: "s",
+							category: "Classical mechanics",
+							difficulty: 4,
+							length: 10,
+							target: 2,
+							creator_id: 11, 
+							content: "Point A moves uniformly with velocity $v=3 m/s$ so that the vector $v$
+is continually 'aimed' at point B which in its turn moves recti-
+linearly and uniformly with velocity $u=2 m/s $. At the initial moment of
+time $v$ is $\perp$ $u$ and the points are separated by a distance $l=6$. How soon
+will the points converge?",
+)
+
+
+#4
+Problem.create!(
+							title: "A problem about distance",
+							answer: 579,
+							degree_of_answer: -2,
+							units_of_answer: "m",
+							category: "Classical mechanics",
+							difficulty: 5,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "Two particles, 1 and 2, move with constant velocities $v_1=10m/s$
+and $v_2=13 m/s$ along two mutually perpendicular straight lines toward the
+intersection point $ O $. At the moment $t = 0$ the particles were located
+at the distances $1_1=15 m $ and $1_2=10 m$ from the point $O$. What is the smallest distance between the particles equal to?",
+)
+#5
+Problem.create!(
+							title: "Off road",
+							answer: 224,
+							degree_of_answer: 0,
+							units_of_answer: "m",
+							category: "Classical mechanics",
+							difficulty: 3,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							#picture: open("")
+							content: "From point $A$ located on a highway (see figure) one has to get
+by car as soon as possible to point $B$ located in the field at a distance $l=200 m$
+from the highway. It is known that the car moves in the field $k= 1,5$
+times slower than on the highway. At what distance from point $D$
+one must turn off the highway?",
+)
+
+#6
+Problem.create!(
+							title: "Bouncing ball on an inclined plane",
+							answer: 4,
+							degree_of_answer: 0,
+							units_of_answer: "m",
+							category: "Classical mechanics",
+							difficulty: 3,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "A ball starts falling with zero initial velocity on a smooth
+inclined plane forming an angle $\alpha =30^o$ with the horizontal. Having fall-
+en the distance $h=1 m$, the ball rebounds elastically off the inclined plane.
+At what distance from the impact point will the -ball rebound for
+the second time?",
+)
+
+#7
+Problem.create!(
+							title: "Rotations",
+							answer: 12,
+							degree_of_answer: 0,
+							units_of_answer: "rad/s^2",
+							category: "Classical mechanics",
+							difficulty: 2,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "Two solid bodies rotate about stationary mutually perpen-
+dicular intersecting axes with constant angular velocities $\omega_1=
+3.0 rad/s$ and $\omega_2 = 4.0 rad/s$. Find the angular
+ acceleration of one body relative to the other.",
+)
+
+#8
+Problem.create!(
+							title: "A motor and a bar",
+							answer: 729,
+							degree_of_answer: -2,
+							units_of_answer: "s",
+							category: "Classical mechanics",
+							difficulty: 1,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "A horizontal plane with the coefficient of friction $k=0.75$ supports
+two bodies: a bar and an electric motor with a battery on a block.
+A thread attached to the bar is wound on the shaft of the electric
+motor. The distance between the bar and the electric motor is equal
+to $l=3 m$. When the motor is switched on, the bar, whose mass is twiceas great as that of the other body, starts moving with a constant ac-
+celeration $a=0.5 m/s$. How soon will the bodies collide?",
+)
+#9
+Problem.create!(
+							title: "Bals , rods etc.",
+							answer: 140,
+							degree_of_answer: -2,
+							units_of_answer: "s",
+							category: "Classical mechanics",
+							difficulty: 2,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "In the arrangement shown in Figure the mass of ball 1
+is = 1.8 times as great as that of rod 2. The length of the latter is
+$l = 100 cm$. The masses of the pulleys and the threads, as well as
+the friction, are negligible. The ball is set on the same level as the
+lower end of the rod and then released. How soon will the ball be
+opposite the upper end of the rod?",
+)
+
+#10
+Problem.create!(
+							title: "Bals , rods etc. 2",
+							answer: 60,
+							degree_of_answer: -2,
+							units_of_answer: "s",
+							category: "Classical mechanics",
+							difficulty: 3,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "In the arrangement shown in the Figure the mass of $body_1$
+is $z = 4.0$ times as great as that of $body_2$. The height $h = 20 cm$.
+The masses of the pulleys and the threads, as well as the friction,
+are negligible. At a certain moment $body_2$ is released and the arrange-
+ment set in motion. What is the maximum height that $body_2$ will
+go up to?",
+)
+#11
+Problem.create!(
+							title: "Bars 2",
+							answer: 643,
+							degree_of_answer: -2,
+							units_of_answer: "m/s^2",
+							category: "Classical mechanics",
+							difficulty: 3,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "In the arrangement shown in the Figure the masses of the
+wedge $M$ and the body $m= \frac{M}{2}$ are known. The appreciable friction exists
+only between the wedge and the body $m$, the friction coefficient being
+equal to $k=0.65$. The masses of the pulley and the thread are negligible.
+Find the acceleration of the body $m$ relative to the horizontal surface
+on which the wedge slides.",
+)
+#12
+Problem.create!(
+							title: "Bars",
+							answer: 999,
+							degree_of_answer: -3,
+							units_of_answer: "m / s^2",
+							category: "Classical mechanics",
+							difficulty: 5,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "Prism 1 of mass $m_1=4 kg$ and with angle $\alpha=30^o$ (see the Figure) rests on
+a horizontal surface. $Bar_2$ of ass $m_2=1 kg$ is placed on the prism. Assum-
+ing the friction to be negligible, find the acceleration of the prism.",
+)
+#13
+Problem.create!(
+							title: "Swing ball",
+							answer: 53,
+							degree_of_answer: 0,
+							units_of_answer: "degrees",
+							category: "Classical mechanics",
+							difficulty: 1,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "A ball suspended by a thread swings in a vertical plane so
+that its acceleration values in the extreme and the lowest position
+are equal. Find the thread deflection angle in the extreme position.",
+)
+
+#14
+Problem.create!(
+							title: "Sliding on a sphere",
+							answer: 181,
+							degree_of_answer: -2,
+							units_of_answer: "m / s",
+							category: "Classical mechanics",
+							difficulty: 2,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "A small body A starts sliding off the top of a smooth sphere
+of radius $R=50 cm$ (see Figure). Find the break-off veloc-
+ity of the body.",
+)
+#15
+Problem.create!(
+							title: "Sliding car",
+							answer: 579,
+							degree_of_answer: -2,
+							units_of_answer: "m",
+							category: "Classical mechanics",
+							difficulty: 1,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "A car moves with a constant tangential acceleration $\omega_t =
+0.62 m / s^2$ along a horizontal surface circumscribing a circle of
+radius $R = 40 m$. The coefficient of sliding friction between the
+wheels of the car and the surface is $k = 0.20$. What distance will
+the car ride without sliding if at the initial moment of time its veloc-
+ity is equal to zero?",
+)
+
+#16
+Problem.create!(
+							title: "Chain on a cone",
+							answer: 120,
+							degree_of_answer: -2,
+							units_of_answer: "N",
+							category: "Classical mechanics",
+							difficulty: 1,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "A chain of mass $m=200 g$ forming a circle of radius $R=30 cm$ is slipped on a
+smooth round cone with half-angle $\theta=15^o$. Find the tension of the chain
+if it rotates with a constant angular velocity $\omega=2 rad / s$ about a vertical axis
+coinciding with the symmetry axis of the cone.",
+)
+
+#17
+Problem.create!(
+							title: "Inside a cylinder",
+							answer: 795,
+							degree_of_answer: -3,
+							units_of_answer: "N",
+							category: "Classical mechanics",
+							difficulty: 4,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "A particle of mass $m=50g$ moves along the internal smooth sur-
+face of a vertical cylinder of radius $R=50 cm$. Find the force with which the
+particle acts on the cylinder wall if at the initial moment of time
+its velocity equals $v_o=3 m/s$ and forms an angle $\alpha=20^o$ with the horizontal.",
+)
+
+#18
+Problem.create!(
+							title: "Panzer",
+							answer: 206,
+							degree_of_answer: -7,
+							units_of_answer: "s",
+							category: "Classical mechanics",
+							difficulty: 3,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "Having gone through a plank of thickness $h=1 cm$, a bullet
+changed its velocity from $v_o=600 m/s$, to $v=400 m/s$. Find the time of motion of the
+bullet in the plank, assuming the resistance force to be proportional
+to the square of the velocity.",
+)
+
+#19
+Problem.create!(
+							title: "Chain on a sphere",
+							answer: 563,
+							degree_of_answer: -2,
+							units_of_answer: "m / s^2",
+							category: "Classical mechanics",
+							difficulty: 3,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "A chain of length $l=40 cm is placed on a smooth spherical surface
+of radius $R=30$ with one of its ends fixed at the top of the sphere. What
+will be the acceleration of each element of the chain when its upper
+end is released? ",
+)
+
+#20
+Problem.create!(
+							title: "Steady motion",
+							answer: 1,
+							degree_of_answer: 0,
+							units_of_answer: "s",
+							category: "Classical mechanics",
+							difficulty: 5,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "A particle moves in a plane under the action of a force
+which is always perpendicular to the particle's velocity and depends
+on a distance to a certain point on the plane as $1/r^n$, where $n$ is a
+constant. What is the minimum value of $n$ for which the motion of the particle along
+the circle will not be steady?",
+)
+Problem.create!(
+							title: "ship and wind",
+							answer: 579,
+							degree_of_answer: -2,
+							units_of_answer: "s",
+							category: "Classical mechanics",
+							difficulty: 4,
+							length: 10,
+							target: 1,
+							creator_id: 11, 
+							content: "",
+)
