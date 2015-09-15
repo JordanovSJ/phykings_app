371946948624215fb6dbfe951bfc8771b992a88a
a-sklyarov <sasho.alex.sk@gmail.com>
2015-09-15 14:40:00 +0300

    edniti minuti

--- config/initializers/global_constants.rb
+++ config/initializers/global_constants.rb
@@ -10,11 +10,11 @@ CATEGORY = ["Classical mechanics",
 						"Relativity", 
 						"Optics"]
 						
-LENGTH = [1, 10, 30, 60]
+LENGTH = [10, 30, 60]
 
 MAX_DIFFICULTY = 10
 
-LENGTH_COMPETITION=[1, 10, 30, 60, 120]
+LENGTH_COMPETITION=[10, 30, 60, 120]
 
 # At every this many votes the problem parameters will refresh
 VOTES_REFRESH = 20
