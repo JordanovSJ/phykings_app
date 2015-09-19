f1f2e2a8482b26752f31a0800a63f4acae53ab3d
a-sklyarov <sasho.alex.sk@gmail.com>
2015-09-20 00:50:07 +0300

    Big fix! New front page. Reorganized the assets.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( html5up/ie8.css html5up/ie9.css application-front.css )
